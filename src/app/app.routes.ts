3836ff0726b1261c4bd915f543c78b19294c375b
malvina-14 <malvinamehmet96@gmail.com>
2024-07-25 21:44:56 +0200

    add the AuthGuard even for the Chart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -43,7 +43,7 @@ export const routes: Routes = [
           import(
             './pages/features/chart-data/chart-data.component'
             ).then((m) => m.ChartDataComponent),
-
+        canActivate: [AuthGuard],
 
       },
     ],
