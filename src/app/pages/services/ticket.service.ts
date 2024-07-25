c02547f3c753af8867fbd52d55366f55a283cabe
malvina-14 <malvinamehmet96@gmail.com>
2024-07-25 13:36:46 +0200

    add the logic of adding the ticket in the service and also include the condition of not allowing the ticket to be added if it is duplicated

--- src/app/pages/services/ticket.service.ts
+++ src/app/pages/services/ticket.service.ts
@@ -26,4 +26,33 @@ export class TicketService {
 
         return collectionRef.valueChanges();
     }
+
+  add(ticket: Ticket): Promise<void> {
+    return this.isDuplicated(ticket).then(isDup => {
+      if (isDup) {
+        console.error('Error adding ticket: Duplicate ticket exists');
+        return Promise.reject('Duplicate ticket exists');
+      } else {
+        return this.ticketsCollection.add(ticket)
+          .then((docRef) => console.log('Ticket added successfully', docRef))
+          .catch((error) => console.error('Error adding ticket: ', error));
+      }
+    });
+  }
+  isDuplicated(ticket: Ticket): Promise<boolean> {
+    return this.ticketsCollection.ref
+      .where('inbound', '==', ticket.inbound)
+      .where('outbound', '==', ticket.outbound)
+      .where('from_date', '==', ticket.from_date)
+      .where('to_date', '==', ticket.to_date)
+      .where('seat_number', '==', ticket.seat_number)
+      .get()
+      .then(querySnapshot => {
+        return !querySnapshot.empty;
+      })
+      .catch(error => {
+        console.error('Error checking for duplicate ticket: ', error);
+        return false;
+      });
+  }
 }
