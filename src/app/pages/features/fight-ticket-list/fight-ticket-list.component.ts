f0536fa00f806d56b7080959b0dc5034f1c145a7
malvina-14 <malvinamehmet96@gmail.com>
2024-07-25 10:54:48 +0200

    add a searchbar based on all properties of the ticket(except the dates) and a filter based on the ticket type

--- src/app/pages/features/fight-ticket-list/fight-ticket-list.component.ts
+++ src/app/pages/features/fight-ticket-list/fight-ticket-list.component.ts
@@ -1,39 +1,38 @@
 import {Component, OnInit} from '@angular/core';
 import {CommonModule, DatePipe} from "@angular/common";
-import {RouterLink, RouterOutlet} from "@angular/router";
+import {Router, RouterLink, RouterOutlet} from "@angular/router";
 import {MdbCheckboxModule} from "mdb-angular-ui-kit/checkbox";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {Ticket} from "../../interfaces/flight-ticket.interface";
-import {FlightTicketAddModalComponent} from "../flight-ticket-add-modal/flight-ticket-add-modal.component";
-import {MatDialog} from "@angular/material/dialog";
-import {FlightTicketViewModalComponent} from "../flight-ticket-view-modal/flight-ticket-view-modal.component";
 import {AuthService} from "../../../auth/services/auth.service";
+import {FlightTicketViewModalComponent} from "../flight-ticket-view-modal/flight-ticket-view-modal.component";
+import {MatDialog} from "@angular/material/dialog";
+import {FlightTicketAddModalComponent} from "../flight-ticket-add-modal/flight-ticket-add-modal.component";
 import {TicketService} from "../../services/ticket.service";
+import {Ticket} from "../../interfaces/flight-ticket.interface";
 
 @Component({
   selector: 'app-fight-ticket-list',
   standalone: true,
   imports: [
     DatePipe,
-    CommonModule,
-    RouterOutlet,
-    MdbCheckboxModule,
-    ReactiveFormsModule,
-    RouterLink,
-    FormsModule,
+    CommonModule, RouterOutlet, MdbCheckboxModule, ReactiveFormsModule, RouterLink, FormsModule
   ],
   templateUrl: './fight-ticket-list.component.html',
   styleUrl: './fight-ticket-list.component.scss'
 })
-export class FightTicketListComponent implements OnInit {
+export class FightTicketListComponent implements OnInit{
   userRole: string = 'user';
 
   tickets: Ticket[] = [];
+  filteredTickets: Ticket[] = [];
+  searchTerm: string = '';
+  selectedType: string = 'All';
+  ticketTypes: string[] = ['All','Economy', 'Business', 'First Class'];
 
-  constructor(public dialog: MatDialog,private authService: AuthService,private ticketService: TicketService) {
-  }
+  constructor(private router: Router,private authService: AuthService,public dialog: MatDialog, private ticketService: TicketService) {}
 
   ngOnInit(): void {
+    this.filteredTickets = this.tickets;
     const user = this.authService.getUser()
     if (Object.keys(user).length) {
       this.userRole = user.role;
@@ -43,34 +42,48 @@ export class FightTicketListComponent implements OnInit {
     }
 
   }
+
+  onTicketTypeChange(selectedType: string): void {
+    this.getTickets()
+  }
+
   getTickets(){
-    this.ticketService.tickets().subscribe(tickets => {
+    this.ticketService.tickets(this.selectedType).subscribe(tickets => {
       this.tickets = tickets.map(ticket => ({
         ...ticket,
 
       }));
-      this.tickets = [...this.tickets]
-      console.log('Tickets:', this.tickets);
+      this.filteredTickets = [...this.tickets]
     });
   }
+
+
+  searchTickets(): void {
+    this.filteredTickets = this.tickets.filter(ticket =>
+      ticket.inbound.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      ticket.outbound.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      ticket.ticket_type.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      ticket.ticket_type_id.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      ticket.seat_number.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
 
   addTicket(): void {
     const dialogRef = this.dialog.open(FlightTicketAddModalComponent, {
       width: '600px'
     });
 
-    dialogRef.afterClosed().subscribe((result: Ticket) => {
+    dialogRef.afterClosed().subscribe(result => {
       if (result) {
-        this.tickets.push(result);
+        this.filteredTickets.push(result);
       }
     });
   }
-
   viewTicket(ticket: any) {
-    console.log('ticket', ticket)
     this.dialog.open(FlightTicketViewModalComponent, {
       width: '600px',
-      data: {ticket},
+      data: { ticket },
     });
   }
 
